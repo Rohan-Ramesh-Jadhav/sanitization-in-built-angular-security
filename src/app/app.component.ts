553a1b4948f7bfb8171001eff9b68562af2eb247
rohan <rohanjdhv550@gmail.com>
2022-07-08 11:53:40 +0530

    Sanitization in case of security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer,SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sanitization-in-built-angular-security';
+
+  injectedText:string = "this is example of sanitization code <script>alert('attack!!')</script>";
+  hrefCode='';
+  sanitizerTrust:SafeUrl='';
+  constructor(private sanitizer:DomSanitizer){
+    this.hrefCode = 'javascript:alert("Hi there")';
+    this.sanitizerTrust = sanitizer.bypassSecurityTrustUrl(this.injectedText)
+  }
+  
 }
